1a3e52fa21e3986352f460422ddf9f8c672e407a
Blador <minhhung6410@gmail.com>
2021-09-29 00:15:35 +0700

    upload project 2 again

--- Project2-Commerce/commerce/auctions/models.py
+++ Project2-Commerce/commerce/auctions/models.py
@@ -5,11 +5,22 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class actions_user(models.Model):
-    username = models.CharField(max_length=64, unique= True)
-    password = models.CharField(max_length=64)
-    email = models.EmailField(max_length=254, unique= True)
+class Category(models.Model):
+    category = models.CharField(max_length=64)
 
-    USERNAME_FIELD = 'username'
-    # def __str__(self):
-    #     return self.username
+    def __str__(self):
+        return f"{self.category}"
+
+class Listings(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    current_price = models.DecimalField(max_digits=2)
+    image_url = models.URLField()
+    # create_user = models.CharField(max_length=100)
+    # date_created = models.Date()
+    
+class Bids(models.Model):
+    pass
+
+class Comments(models.Model):
+    pass
