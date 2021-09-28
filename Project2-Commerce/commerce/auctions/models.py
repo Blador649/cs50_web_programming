3663e2b3d5b2500dc5ec8dceb5d91ed9188520eb
Your Name <you@example.com>
2021-09-28 23:08:20 +0700

    user access

--- Project2-Commerce/commerce/auctions/models.py
+++ Project2-Commerce/commerce/auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class actions_user(models.Model):
+    username = models.CharField(max_length=64, unique= True)
+    password = models.CharField(max_length=64)
+    email = models.EmailField(max_length=254, unique= True)
+
+    USERNAME_FIELD = 'username'
+    # def __str__(self):
+    #     return self.username
